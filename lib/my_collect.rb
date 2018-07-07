1cef25de4fb4e0d80442db20871555455223a997
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-07 17:03:38 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,3 +5,7 @@ def my_collect(array)
     i += 1
   end
 end
+
+languages = ["Ruby", "Javascript", "Python", "Objective-C"]
+my_collect(languages)  {|language| puts language}
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
